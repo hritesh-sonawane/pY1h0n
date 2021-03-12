2aa18622f999384a0caff2268a4ac3a7920559d4
DESKTOP-MQS8GBN\hr7te <hriteshbebop@gmail.com>
2021-03-13 00:43:16 +0530

    numbers commit

---
+++ Snippets/1-numbers.py
@@ -0,0 +1,42 @@
+# Integers in binary, octal and hexadecimal formats
+# binary
+a = 0b10111011
+print(a)
+# Prints 187
+print(type(a))
+# Prints <class 'int'>
+
+# octal
+print(0o10)
+# Prints 8
+
+# hex
+print(0xFF)
+# Prints 255
+
+
+# Boolean in python
+x = True
+x = False
+print(type(x))
+# Prints <class 'bool'>
+
+
+# Floating point number
+x = 10.1
+print(type(x))
+# Prints <class 'float'>
+
+
+# Complex numbers
+x = 3+4j
+print(type(x))
+# Prints <class 'complex'>
+
+# real part
+print(x.real)
+# Prints 3.0
+
+# imaginary part
+print(x.imag)
+# Prints 4.0
