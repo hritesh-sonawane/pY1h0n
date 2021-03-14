e9f50d14d567acb62c28c2fe0269b27b3fd6f7bc
DESKTOP-MQS8GBN\hr7te <hriteshbebop@gmail.com>
2021-03-14 12:41:47 +0530

    while commit

---
+++ Snippets/10-while.py
@@ -0,0 +1,10 @@
+# 1 liner while
+count = 0
+while count <= 3: print(count); count += 1
+
+print("Ready for takeoff?")
+countdown = 10
+while countdown >= 0: 
+  print(countdown)
+  countdown -= 1
+print("We have liftoff!")
