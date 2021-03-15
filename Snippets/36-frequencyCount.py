5a29cf32c810cb828e2ef832e0af2bc5c6f21d99
DESKTOP-MQS8GBN\hr7te <hriteshbebop@gmail.com>
2021-03-15 18:34:02 +0530

    frequency of each element

---
+++ Snippets/36-frequencyCount.py
@@ -0,0 +1,12 @@
+def frequency_dictionary(words):
+  freqs = {}
+  for word in words:
+    if word not in freqs:
+    	freqs[word] = 0
+    freqs[word] += 1
+  return freqs
+
+print(frequency_dictionary(["apple", "apple", "cat", 1]))
+# should print {"apple":2, "cat":1, 1:1}
+print(frequency_dictionary([0,0,0,0,0]))
+# should print {0:5}
