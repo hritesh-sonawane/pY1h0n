7fda9fa3ceef4362af0a2113f164f1aa473a2e97
DESKTOP-MQS8GBN\hr7te <hriteshbebop@gmail.com>
2021-03-16 01:07:18 +0530

    decorators w/ params commit

--- Snippets/47-decorator.py
+++ Snippets/47-decorator.py
@@ -1,7 +1,7 @@
 def title_decorator(print_name_function):
-  def wrapper():
+  def wrapper(*args, **kwargs):
     print("Professor:")
-    print_name_function()
+    print_name_function(*args, **kwargs)
   return wrapper
 
 @title_decorator
@@ -9,8 +9,8 @@ def print_my_name():
   print("Ryuk")
 
 @title_decorator
-def print_Ls_name():
-  print("L")
+def print_Ls_name(name):
+  print(name)
 
-print_Ls_name()
+print_Ls_name("L")
 print_my_name()
