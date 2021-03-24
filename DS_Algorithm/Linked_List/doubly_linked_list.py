6101ea991f56e8c576e15179fe121b7ab359b9d6
DESKTOP-MQS8GBN\hr7te <hriteshbebop@gmail.com>
2021-03-24 21:58:56 +0530

    add add_to_head

--- DS_Algorithm/Linked_List/doubly_linked_list.py
+++ DS_Algorithm/Linked_List/doubly_linked_list.py
@@ -22,5 +22,18 @@ class Node:
 
 
 class DoublyLinkedList:
-    pass
+    def __init__(self):
+        self.head_node = None
+        self.tail_node = None
 
+    def add_to_head(self, new_value):
+        new_head = Node(self.new_value)
+        current_head = self.head_node
+
+        if current_head != None:
+            current_head.set_prev_node(new_head)
+            new_head.set_next_node(current_head)
+        self.head_node = new_head
+
+        if self.tail_node == None:
+            self.tail_node = new_head
