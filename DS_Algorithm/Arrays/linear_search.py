5c0b65447af40003f72948e7d1c00543ff3edf03
DESKTOP-MQS8GBN\hr7te <hriteshbebop@gmail.com>
2021-03-31 22:17:57 +0530

    ✔  update: linear_search returns duplicates too

--- DS_Algorithm/Arrays/linear_search.py
+++ DS_Algorithm/Arrays/linear_search.py
@@ -1,12 +1,16 @@
-number_list = [ 10, 14, 19, 26, 27, 31, 33, 35, 42, 44]
+# duplicates in linear search (here, 33)
+number_list = [ 33, 10, 14, 19, 26, 27, 31, 33, 35, 42, 44]
 target_number = 33
 
 def linear_search(search_list, target_value):
+    matches = []
     for idx in range(len(search_list)):
-
         if search_list[idx] == target_value:
-            return idx
-    raise ValueError("{0} not in list".format(target_value))
+            matches.append(idx)
+    if matches:
+        return matches
+    else:
+        raise ValueError("{0} not in list".format(target_value))
 
 try:
     result = linear_search(number_list, target_number)
