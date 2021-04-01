4e922704438ff06f39fbbbf16959a8db81400040
DESKTOP-MQS8GBN\hr7te <hriteshbebop@gmail.com>
2021-04-01 22:40:26 +0530

    👽  add base case 2

--- DS_Algorithm/Arrays/binary_search.py
+++ DS_Algorithm/Arrays/binary_search.py
@@ -3,10 +3,13 @@ def binary_search(sorted_list, target):
         return 'value not found'
     mid_idx = len(sorted_list)//2
     mid_val = sorted_list[mid_idx]
-    return mid_idx, mid_val
+
+    if mid_val == target:
+        return mid_idx
 
 
-# For testing:
+# base case for when we DO find the value.
 sorted_values = [13, 14, 15, 16, 17]
 print(binary_search([], 42))
 print(binary_search(sorted_values, 42))
+print(binary_search(sorted_values, 15))
