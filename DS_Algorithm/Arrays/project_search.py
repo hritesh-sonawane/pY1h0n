e9b9ebcffbe1040b1d236ea5ce34bc91f5b8d8b9
DESKTOP-MQS8GBN\hr7te <hriteshbebop@gmail.com>
2021-04-02 23:51:46 +0530

    📌  add project_search

---
+++ DS_Algorithm/Arrays/project_search.py
@@ -0,0 +1,49 @@
+def sparse_search(data, search_val):
+    print("Data: " + str(data))
+    print("Search Value: " + str(search_val))
+    first, last = 0, len(data) - 1
+
+    while first <= last:
+        mid = (first + last) // 2
+
+        if not data[mid]:
+            left = mid - 1
+            right = mid + 1
+
+            while True:
+                if left < first and right > last:
+                    print(str(search_val) + " is not in the dataset")
+                    return
+                elif right <= last and data[right]:
+                    mid = right
+                    break
+                elif left >= first and data[left]:
+                    mid = left
+                    break
+
+                right += 1
+                left -= 1
+
+        if data[mid] == search_val:
+            print(str(search_val) + " found at position " + str(mid) + ".")
+            return
+        elif search_val < data[mid]:
+            last = mid - 1
+        elif search_val > data[mid]:
+            first = mid + 1
+
+    print(str(search_val) + " is not in the dataset.")
+
+
+# Test cases:
+sparse_search(["A", "", "", "", "B", "", "", "", "C", "", "", "D"], "X")
+print("---------------")
+sparse_search(["A", "B", "", "", "E"], "A")
+print("---------------")
+sparse_search(["", "X", "", "Y", "Z"], "Z")
+print("---------------")
+sparse_search(["A", "", "", "", "B", "", "", "", "C"], "D")
+print("---------------")
+sparse_search(["Apple", "", "Banana", "", "", "", "", "Cow"], "Banana")
+print("---------------")
+sparse_search(["Alex", "", "", "", "", "Devan", "", "", "Elise", "", "", "", "Gary", "", "", "Mimi", "", "", "Parth", "", "", "", "Zachary"], "Parth")
