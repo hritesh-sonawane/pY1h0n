be1c50b0ecee01618540982e8ed1bfe565fc4f77
DESKTOP-MQS8GBN\hr7te <hriteshbebop@gmail.com>
2021-04-03 15:51:49 +0530

    ➕  add queue size

--- Linear_DS/Queues/queue.py
+++ Linear_DS/Queues/queue.py
@@ -1,9 +1,26 @@
 from node import Node
 
 class Queue:
-    def __init__(self):
+    def __init__(self, max_size=None):
         self.head = None
         self.tail = None
+        self.max_size = max_size
+        self.size = 0
 
     def peek(self):
-        return self.head.get_value()
+        if self.size > 0:
+            return self.head.get_value()
+        else:
+            print("Nothing to see here")
+
+    def get_size(self):
+        return self.size
+
+    def has_space(self):
+        if self.max_size == None:
+            return True
+        else:
+            return self.max_size > self.get_size()
+
+    def is_empty(self):
+        return self.size == 0
