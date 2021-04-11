dc7cb783a069cb68bd0aadd09f16c1c89fdd0842
DESKTOP-MQS8GBN\hr7te <hriteshbebop@gmail.com>
2021-04-11 22:22:44 +0530

    🧿  add HashMap class, hash fn

---
+++ Hash Maps/script.py
@@ -0,0 +1,10 @@
+class HashMap:
+    def __init__(self, array_size):
+        self.array_size = array_size
+        self.array = [None for item in range(array_size)]
+
+    def hash(self, key):
+        key_bytes = key.encode()
+        # .encode() is a string method that converts a string into its corresponding bytes
+        hash_code = sum(key_bytes)
+        return hash_code
