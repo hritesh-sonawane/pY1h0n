d58b8c05d6ae353840bab637de4d8e74056696d5
DESKTOP-MQS8GBN\hr7te <hriteshbebop@gmail.com>
2021-04-14 20:43:05 +0530

    🎴  add Open Addressing in getter

--- Hash Maps/script.py
+++ Hash Maps/script.py
@@ -20,11 +20,11 @@ class HashMap:
 
         if current_array_value is None:
             self.array[array_index] = [key, value]
-        return
+            return
 
         if current_array_value[0] == key:
             self.array[array_index] = [key, value]
-        return
+            return
 
         # Collision!
 
@@ -37,11 +37,11 @@ class HashMap:
 
             if current_array_value is None:
                 self.array[new_array_index] = [key, value]
-            return
+                return
 
             if current_array_value[0] == key:
                 self.array[new_array_index] = [key, value]
-            return
+                return
 
             number_collisions += 1
 
@@ -55,8 +55,26 @@ class HashMap:
 
         if possible_return_value is None:
             return None
-        elif possible_return_value[0] == key:
+
+        if possible_return_value[0] == key:
             return possible_return_value[1]
+
+        retrieval_collisions = 1
+
+        while (possible_return_value != key):
+            new_hash_code = self.hash(key, retrieval_collisions)
+            retrieving_array_index = self.compressor(new_hash_code)
+            possible_return_value = self.array[retrieving_array_index]
+
+            if possible_return_value is None:
+                return None
+
+            if possible_return_value[0] == key:
+                return possible_return_value[1]
+
+        retrieval_collisions += 1
+
+        return
 
 
 # Testing
