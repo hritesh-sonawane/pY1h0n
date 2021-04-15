794dd698d3bf7a8b4ef1e16e1f8c1f9fd1c41ceb
DESKTOP-MQS8GBN\hr7te <hriteshbebop@gmail.com>
2021-04-15 21:53:37 +0530

    🏹  add BlossomMap & ♻

---
+++ Hash Maps/BlossomMap/hashMap.py
@@ -0,0 +1,3 @@
+# Give people lightning fast access to all of the possible meanings of their favorite flowers.
+# Implement a hash map to relate the names of flowers to their meanings.
+# In order to avoid collisions when hashing function collides with the names of two flowers, use separate chaining.
