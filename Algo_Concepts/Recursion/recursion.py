f68a88e94f1defa785c552b2724795efd70105dd
Hritesh Sonawane <hriteshbebop@gmail.com>
2021-04-21 16:35:34 +0530

    🧿 add basic recursive logic

--- Algo_Concepts/Recursion/recursion.py
+++ Algo_Concepts/Recursion/recursion.py
@@ -1,18 +1,25 @@
-# Building a custom call stack
+# def sum_to_one(n):
+#     result = 1
+#     call_stack = []
+#     while n != 1:
+#         execution_context = { "n_value": n }
+#         call_stack.append(execution_context)
+#         n -= 1
+#         print(call_stack)
+#     print("BASE CASE REACHED")
+#     while len(call_stack) != 0:
+#         return_value = call_stack.pop()
+#         print("Return value of {0} adding to result {1}".format(return_value['n_value'], result))
+#         result += return_value['n_value']
+#     return result, call_stack
+
+# sum_to_one(4)
 
 def sum_to_one(n):
-    result = 1
-    call_stack = []
-    while n != 1:
-        execution_context = { "n_value": n }
-        call_stack.append(execution_context)
-        n -= 1
-        print(call_stack)
-    print("BASE CASE REACHED")
-    while len(call_stack) != 0:
-        return_value = call_stack.pop()
-        print("Return value of {0} adding to result {1}".format(return_value['n_value'], result))
-        result += return_value['n_value']
-    return result, call_stack
+  if n == 1:
+    return n
+  else:
+    print("Recursing with input: {0}".format(n))
+    return n + sum_to_one(n-1)
 
-sum_to_one(4)
+print(sum_to_one(7))
